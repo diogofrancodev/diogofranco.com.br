11eb9de02bfc6b9ca93612209270ad1d8f819eea
Franco <df7dev@gmail.com>
2024-04-12 02:54:15 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>DF - Desenvolvimento Web</title>
         @laravelPWA
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
