3072a6ead917d3525b4325c9950a2e5b6ad2b83c
Franco <df7dev@gmail.com>
2024-04-12 02:50:48 -0300

    mudando direção dos icons para build do vite

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,16 +1,16 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
     '/offline',
-    '/css/app.css',
-    '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/build/assets/app.css',
+    '/build/assets/app.js',
+    '/build/images/icons/icon-72x72.png',
+    '/build/images/icons/icon-96x96.png',
+    '/build/images/icons/icon-128x128.png',
+    '/build/images/icons/icon-144x144.png',
+    '/build/images/icons/icon-152x152.png',
+    '/build/images/icons/icon-192x192.png',
+    '/build/images/icons/icon-384x384.png',
+    '/build/images/icons/icon-512x512.png',
 ];
 
 // Cache on install
